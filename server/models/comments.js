209fbadbe39bfb7056798c711da059cf5013aae0
Natalia <natorgom@gmail.com>
2019-03-02 12:12:24 +0100

    add comments model

---
+++ server/models/comments.js
@@ -0,0 +1,28 @@
+const mongoose = require('../db');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  comment: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Number,
+    required: true
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
